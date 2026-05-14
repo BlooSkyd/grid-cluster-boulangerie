13caa0d9d739b6c2f15a3b6c941d31afa467c260
Clement Taurand <clment2402@gmail.com>
2026-05-14 22:23:15 +0200

    fix api read and added db_slave testing values

--- api_read/read-api.py
+++ api_read/read-api.py
@@ -8,12 +8,12 @@ def get_conn():
     # Connexion au SLAVE (Lecture seule)
     return psycopg2.connect("host=db_slave dbname=boulangerie user=user password=pass")
 
-@app.get("/pains")
+@app.get("/top")
 def get_all_pains():
     conn = get_conn()
     # RealDictCursor permet d'avoir les noms des colonnes dans la réponse JSON
     cur = conn.cursor(cursor_factory=RealDictCursor)
-    cur.execute("SELECT * FROM pains ORDER BY id DESC")
+    cur.execute("SELECT * FROM v_top3_ventes;")
     results = cur.fetchall()
     cur.close()
     conn.close()
@@ -23,7 +23,7 @@ def get_all_pains():
 def search_pains(nom: str):
     conn = get_conn()
     cur = conn.cursor(cursor_factory=RealDictCursor)
-    cur.execute("SELECT * FROM pains WHERE nom ILIKE %s", (f'%{nom}%',))
+    cur.execute("SELECT * FROM compta WHERE nom ILIKE %s", (f'%{nom}%',))
     results = cur.fetchall()
     cur.close()
     conn.close()
--- postgres/init_slave.sql
+++ postgres/init_slave.sql
@@ -6,9 +6,15 @@ CREATE TABLE IF NOT EXISTS compta (
     nb_cmd INTEGER DEFAULT 0
 );
 
+INSERT INTO compta (ref_id, nom, prix_total, nb_cmd) values
+(1, 'Baguette Tradition', 45.0, 11),
+(2, 'Pain de Campagne', 4.0, 5),
+(3, 'Baguette Blanche', 15.0, 15),
+(4, 'Pain aux Céréales', 145.0, 20);
+
 -- Vue pour obtenir le top 5 des ventes par prix_total
-CREATE OR REPLACE VIEW v_top5_ventes AS
+CREATE OR REPLACE VIEW v_top3_ventes AS
 SELECT ref_id, nom, prix_total, nb_cmd
 FROM compta
 ORDER BY prix_total DESC
-LIMIT 5;
+LIMIT 3;
