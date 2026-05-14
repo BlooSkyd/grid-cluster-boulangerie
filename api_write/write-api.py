2a8d643f4057745a3c5f5fdb81c502d2cb728bce
Clement Taurand <clment2402@gmail.com>
2026-05-14 18:57:33 +0200

    restore lost version

--- api_write/write-api.py
+++ api_write/write-api.py
@@ -1,34 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import os
 import json
 from kafka import KafkaProducer
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
-app = FastAPI(title="API Écriture Pains (producer)")
+app = FastAPI(title="API Écriture Commandes")
 
-class Pain(BaseModel):
-    nom: str
-    cuisson: str
-    prix: float
-    poids: int
+class Commande(BaseModel):
+    ref_id: int
+    qte: int
 
-KAFKA_SERVERS = os.environ.get("KAFKA_BOOTSTRAP_SERVERS", "kafka1:9092")
+KAFKA_SERVERS = os.environ.get("KAFKA_BOOTSTRAP_SERVERS", "kafka1:9092") # a revoir les broker ne sont pas tous utilisé
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_SERVERS.split(","),
     value_serializer=lambda v: json.dumps(v).encode("utf-8"),
 )
 
-@app.post("/pains")
-def create_pain(pain: Pain):
-    event = {"operation": "create", "data": pain.dict()}
-    producer.send("pains", event)
-    producer.flush()
-    return {"message": f"Événement envoyé pour '{pain.nom}'"}
+@app.post("/commandes")
+def create_commande(commande: Commande):
+    if commande.qte <= 0:
+        raise HTTPException(status_code=422, detail="qte doit être un entier strictement positif")
 
-
-@app.delete("/pains/{pain_id}")
-def delete_pain(pain_id: int):
-    event = {"operation": "delete", "id": pain_id}
-    producer.send("pains", event)
-    producer.flush()
-    return {"message": "Événement de suppression envoyé."}
+    else:
+        payload = commande.dict()
+        event = {"operation": "create", "entity": "commande", "data": payload}
+        producer.send("commandes", event)
+        producer.flush()
+        return {"message": f"Événement de création de commande envoyé pour ref_id '{commande.ref_id}'", "ref_id": commande.ref_id}
